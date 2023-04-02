d5a164a7d6ad3a0dda5b913312ae6a175097f375
larenz jackson <romanlegion15@gmail.com>
2023-04-02 16:37:20 -0500

    added bool to text

--- Character-Pack-Antiquity/Client_PresentSettingsUI.lua
+++ Character-Pack-Antiquity/Client_PresentSettingsUI.lua
@@ -10,16 +10,22 @@ function Client_PresentSettingsUI(rootParent)
 
 		
 	if (Mod.Settings.Unitdata[i].Maxunits == 0) then goto next end
+local Shared = 'No'
+local Vis = 'No'
+local MaxServer = 'No'
+
+			if (Mod.Settings.Unitdata[i].Shared == true)then Shared = 'yes' end
+			if (Mod.Settings.Unitdata[i].Visible == true)then Vis = 'yes' end
+			if (Mod.Settings.Unitdata[i].MaxServer == true)then MaxServer = 'yes' end
+
 
 	UI.CreateLabel(vert).SetText('Unir type ' .. i .. ': ' .. Mod.Settings.Unitdata[i].Name ).SetColor('#FEFF9B')
 	UI.CreateLabel(vert).SetText('\n cost: ' .. Mod.Settings.Unitdata[i].unitcost)
 	UI.CreateLabel(vert).SetText('power in armies: ' .. Mod.Settings.Unitdata[i].unitpower);
 	UI.CreateLabel(vert).SetText('Max amount at once: ' .. Mod.Settings.Unitdata[i].Maxunits);
-	UI.CreateLabel(vert).SetText('Shared Max between players: ' .. Mod.Settings.Unitdata[i].Shared);
-
-	UI.CreateLabel(vert).SetText('Shared Max between players: ' .. Mod.Settings.Unitdata[i].Shared);
-	UI.CreateLabel(vert).SetText('Visible to all players: ' .. Mod.Settings.Unitdata[i].Visible);
-	UI.CreateLabel(vert).SetText('Max useage over game: ' .. Mod.Settings.Unitdata[i].MaxServer);
+	UI.CreateLabel(vert).SetText('Shared Max between players: ' .. Shared);
+	UI.CreateLabel(vert).SetText('Visible to all players: ' .. Vis);
+	UI.CreateLabel(vert).SetText('Max useage over game: ' .. MaxServer);
 
 
 
