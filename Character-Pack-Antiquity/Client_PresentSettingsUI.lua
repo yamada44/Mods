f588d494b33fa1834622bb4d7e196facbe7b3fb7
larenz jackson <romanlegion15@gmail.com>
2023-04-02 16:31:21 -0500

    Update Client_PresentSettingsUI.lua

--- Character-Pack-Antiquity/Client_PresentSettingsUI.lua
+++ Character-Pack-Antiquity/Client_PresentSettingsUI.lua
@@ -12,9 +12,10 @@ function Client_PresentSettingsUI(rootParent)
 	if (Mod.Settings.Unitdata[i].Maxunits == 0) then goto next end
 
 	UI.CreateLabel(vert).SetText('Unir type ' .. i .. ': ' .. Mod.Settings.Unitdata[i].Name ).SetColor('#FEFF9B')
-	UI.CreateLabel(vert).SetText('\n\n cost: ' .. Mod.Settings.Unitdata[i].unitcost)
+	UI.CreateLabel(vert).SetText('\n cost: ' .. Mod.Settings.Unitdata[i].unitcost)
 	UI.CreateLabel(vert).SetText('power in armies: ' .. Mod.Settings.Unitdata[i].unitpower);
 	UI.CreateLabel(vert).SetText('Max amount at once: ' .. Mod.Settings.Unitdata[i].Maxunits);
+
 	UI.CreateLabel(vert).SetText('Shared Max between players: ' .. Mod.Settings.Unitdata[i].Shared);
 	UI.CreateLabel(vert).SetText('Visible to all players: ' .. Mod.Settings.Unitdata[i].Visible);
 	UI.CreateLabel(vert).SetText('Max useage over game: ' .. Mod.Settings.Unitdata[i].MaxServer);
