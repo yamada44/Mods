0eac2747598ff3396f98e7d0614e37e05a40fae8
larenz jackson <romanlegion15@gmail.com>
2023-04-09 15:38:28 -0500

    Update Client_PresentConfigureUI.lua

--- Character-Pack-Antiquity/Client_PresentConfigureUI.lua
+++ Character-Pack-Antiquity/Client_PresentConfigureUI.lua
@@ -60,9 +60,35 @@ end
 
 
 function New()
-	local vert = UI.CreateVerticalLayoutGroup(NewrootParent);
 
-if InputFieldTable[20].test.GetIsChecked() == true then 
+if (InputFieldTable[20].test.GetIsChecked() == false)then 
+	for i = 1, UnitTypeMax do 
+
+		UI.destrpy(InputFieldTable[i].text1)
+		UI.destrpy(InputFieldTable[i].text2)
+		UI.destrpy(InputFieldTable[i].text3)
+		UI.destrpy(InputFieldTable[i].text4)
+		UI.destrpy(InputFieldTable[i].text5)
+		UI.destrpy(InputFieldTable[i].text6)
+		UI.destrpy(InputFieldTable[i].text7)
+		UI.destrpy(InputFieldTable[i].text8)
+		UI.destrpy(InputFieldTable[i].text9)
+		UI.destrpy(InputFieldTable[i].text10)
+		UI.destrpy(InputFieldTable[i].text11)
+		UI.destrpy(InputFieldTable[i].costInputField)
+		UI.destrpy(InputFieldTable[i].powerInputField)
+		UI.destrpy(InputFieldTable[i].maxUnitsField)
+		UI.destrpy(InputFieldTable[i].Image)
+		UI.destrpy(InputFieldTable[i].Shared)
+		UI.destrpy(InputFieldTable[i].Visible)
+		UI.destrpy(InputFieldTable[i].MaxServer)
+		UI.destrpy(InputFieldTable[i].Name)
+
+
+	end
+
+end
+
 
 	for i = 1, UnitTypeMax do -- looping through all the units so you dont have to repeat code
 		if uniteconfig[i] == nil then uniteconfig[i] = {}end -- making sure the tables exist
@@ -98,8 +124,8 @@ if InputFieldTable[20].test.GetIsChecked() == true then
 	local vert = UI.CreateVerticalLayoutGroup(NewrootParent);
 
     local row1 = UI.CreateHorizontalLayoutGroup(vert);
-	UI.CreateLabel(row1).SetText('Unit Type '..i).SetColor('#00D4FF');
-	UI.CreateLabel(row1).SetText('\nHow much gold it costs to buy Unit ' .. i);
+	InputFieldTable[i].text1 = UI.CreateLabel(row1).SetText('Unit Type '..i).SetColor('#00D4FF');
+	InputFieldTable[i].text2 = UI.CreateLabel(row1).SetText('\nHow much gold it costs to buy Unit ' .. i);
 
     InputFieldTable[i].costInputField = UI.CreateNumberInputField(row1)
 		.SetSliderMinValue(1)
@@ -108,14 +134,14 @@ if InputFieldTable[20].test.GetIsChecked() == true then
 
 
 	local row2 = UI.CreateHorizontalLayoutGroup(vert);
-	UI.CreateLabel(row2).SetText('How powerful Unit ' .. i .. ' is (in armies)');
+	InputFieldTable[i].text3 = UI.CreateLabel(row2).SetText('How powerful Unit ' .. i .. ' is (in armies)');
 	InputFieldTable[i].powerInputField = UI.CreateNumberInputField(row2)
 		.SetSliderMinValue(1)
 		.SetSliderMaxValue(30)
 		.SetValue(power);
 
 	local row3 = UI.CreateHorizontalLayoutGroup(vert);
-	UI.CreateLabel(row3).SetText('How many units each player can have at a time\n(Set to 0 to disable this unit)');
+	InputFieldTable[i].text4 = UI.CreateLabel(row3).SetText('How many units each player can have at a time\n(Set to 0 to disable this unit)');
 	InputFieldTable[i].maxUnitsField = UI.CreateNumberInputField(row3)
 		.SetSliderMinValue(0)
 		.SetSliderMaxValue(5)
@@ -124,26 +150,27 @@ if InputFieldTable[20].test.GetIsChecked() == true then
 	
 
 		local row4 = UI.CreateHorizontalLayoutGroup(vert);
-	UI.CreateLabel(row4).SetText('What Image will this Unit have');
+		InputFieldTable[i].text5 = UI.CreateLabel(row4).SetText('What Image will this Unit have');
 	InputFieldTable[i].Image = UI.CreateNumberInputField(row4)
 		.SetSliderMinValue(1)
 		.SetSliderMaxValue(Maxpictures)
 		.SetValue(picture);
 
 		local row6 = UI.CreateHorizontalLayoutGroup(vert);
-		UI.CreateLabel(row6).SetText('Check if you want the Maximum amount to be shared between all players');
+		InputFieldTable[i].text6 = UI.CreateLabel(row6).SetText('Check if you want the Maximum amount to be shared between all players');
 		InputFieldTable[i].Shared = UI.CreateCheckBox(row6).SetIsChecked(shared).SetText('')
 
 		local row7 = UI.CreateHorizontalLayoutGroup(vert);
-		UI.CreateLabel(row7).SetText('Check if you want this unit visible at all times');
+		InputFieldTable[i].text7 = UI.CreateLabel(row7).SetText('Check if you want this unit visible at all times');
+		text7 = UI.CreateLabel(row7).SetText('Check if you want this unit visible at all times');
 		InputFieldTable[i].Visible = UI.CreateCheckBox(row7).SetIsChecked(visible).SetText('')
 
 		local row9 = UI.CreateHorizontalLayoutGroup(vert);
-		UI.CreateLabel(row9).SetText('Check if you want this unit to have a max useage for the entire game');
+		InputFieldTable[i].text8 = UI.CreateLabel(row9).SetText('Check if you want this unit to have a max useage for the entire game');
 		InputFieldTable[i].MaxServer = UI.CreateCheckBox(row9).SetIsChecked(maxserver).SetText('')
 		
 		local row5 = UI.CreateHorizontalLayoutGroup(vert)
-		UI.CreateLabel(row5).SetText('Name of Unit in buy menu');
+		InputFieldTable[i].text9 = UI.CreateLabel(row5).SetText('Name of Unit in buy menu');
 		InputFieldTable[i].Name = UI.CreateTextInputField(vert)
 		.SetPlaceholderText(" Name of Unit Type        ").SetText(name)
 		.SetFlexibleWidth(1)
@@ -151,9 +178,8 @@ if InputFieldTable[20].test.GetIsChecked() == true then
 
 
 		local row8 = UI.CreateHorizontalLayoutGroup(vert)
-		UI.CreateEmpty(row8)
-		UI.CreateLabel(row8).SetText('\n')
+		InputFieldTable[i].text10 = 	UI.CreateEmpty(row8)
+		InputFieldTable[i].text11 = UI.CreateLabel(row8).SetText('\n')
 	
 	end
-end
 end
