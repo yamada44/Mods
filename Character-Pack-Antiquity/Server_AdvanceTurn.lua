b024f9b3b2ba14c160a31497831e0bd92367091b
larenz jackson <romanlegion15@gmail.com>
2023-04-09 22:40:55 -0500

    Update Server_AdvanceTurn.lua

--- Character-Pack-Antiquity/Server_AdvanceTurn.lua
+++ Character-Pack-Antiquity/Server_AdvanceTurn.lua
@@ -10,13 +10,14 @@ function Server_AdvanceTurn_Start(game, addNewOrder)
 			for i,v in pairs (ts.NumArmies.SpecialUnits)do
 		
 				if startsWith(v.ModData, 'C&P') == 'C&P' then 
-		--[[
+		
 					local diebitch = tonumber(string.sub(v.ModData, 4))
 					if diebitch >= Game1.Game.TurnNumber then
-		
+		--[[
 						local mod = WL.TerritoryModification.Create(ts)
 						mod.RemoveSpeicalUnitsOpt = v
-					end]]--
+						]]--
+					end
 				end
 				
 			end
