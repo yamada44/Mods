554a0db1c8a3acfe170900fb376c62bf8b313396
larenz jackson <romanlegion15@gmail.com>
2023-05-02 10:45:43 -0500

    Update Server_AdvanceTurn.lua

--- Character-Pack-Antiquity/Server_AdvanceTurn.lua
+++ Character-Pack-Antiquity/Server_AdvanceTurn.lua
@@ -116,6 +116,7 @@ if type == 3 then local u = Mod.Settings.Unitdata[unittype].MaxServer end
 if type == 4 then local u =  Mod.Settings.Unitdata[unittype].MaxServer end 
 if type == 5 then local u =  Mod.Settings.Unitdata[unittype].MaxServer end
 if type == 6 then local u =  Mod.Settings.Unitdata[unittype].MaxServer end
+if type == nil then local u =  Mod.Settings.Unitdata[unittype].MaxServer end
 
 if type > 20 then local u =  Mod.Settings.Unitdata[unittype].MaxServer end
 if type < 0 then local u =  Mod.Settings.Unitdata[unittype].MaxServer  
