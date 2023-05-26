3acf38ec0e81f65f10ea2dc3f88a64a81f4f9c29
larenz jackson <romanlegion15@gmail.com>
2023-05-25 23:21:49 -0500

    update ingame settings display

--- Character-Pack-Antiquity/Client_PresentSettingsUI.lua
+++ Character-Pack-Antiquity/Client_PresentSettingsUI.lua
@@ -12,16 +12,18 @@ function Client_PresentSettingsUI(rootParent)
 	local MaxServer = 'No'
 	local transfer = 0
 	local level = 0
+	local active = 0
 
 	if (Mod.Settings.Unitdata[i].Maxunits == 0) then goto next end
 
-		imagename(Mod.Settings.Unitdata[i].Image)
+		local image = Imagename(Mod.Settings.Unitdata[i].Image)
 
 				if (Mod.Settings.Unitdata[i].Shared == true)then Shared = 'yes' end
 				if (Mod.Settings.Unitdata[i].Visible == true)then Vis = 'yes' end
 				if (Mod.Settings.Unitdata[i].MaxServer == true)then MaxServer = 'yes' end
 				if (Mod.Settings.Unitdata[i].Transfer ~= nil) then transfer = Mod.Settings.Unitdata[i].Transfer  end
 				if (Mod.Settings.Unitdata[i].Level ~= nil) then level = Mod.Settings.Unitdata[i].Level end
+				if (Mod.Settings.Unitdata[i].Active ~= nil)then active = Mod.Settings.Unitdata[i].Active
 	
 	
 		UI.CreateLabel(vert).SetText('Unit type ' .. i .. ': ' .. Mod.Settings.Unitdata[i].Name ).SetColor('#FEFF9B')
@@ -31,11 +33,12 @@ function Client_PresentSettingsUI(rootParent)
 		UI.CreateLabel(vert).SetText('Min possible age range: ' .. Mod.Settings.Unitdata[i].Minlife);
 		UI.CreateLabel(vert).SetText('Max possible age range: ' .. Mod.Settings.Unitdata[i].Maxlife);
 		UI.CreateLabel(vert).SetText('Unit tranfered upon death Amount: ' .. transfer);
-		UI.CreateLabel(vert).SetText('Base Number of armies needed to level up: ' .. level);
+		UI.CreateLabel(vert).SetText('Base Number of armies needed to kill to level up: ' .. level);
+		UI.CreateLabel(vert).SetText('Unit locked till level: ' .. active);
 		UI.CreateLabel(vert).SetText('Shared Max between players: ' .. Shared);
 		UI.CreateLabel(vert).SetText('Visible to all players: ' .. Vis);
 		UI.CreateLabel(vert).SetText('Max useage over game: ' .. MaxServer);
-		UI.CreateLabel(vert).SetText('Image used: ' .. MaxServer);
+		UI.CreateLabel(vert).SetText('Image used: ' .. image);
 
 	
 
@@ -49,7 +52,7 @@ function Client_PresentSettingsUI(rootParent)
 	
 end
 
-function imagename (name)
+function Imagename (name)
 	local filename = {}
 
 	filename[1] = 'Barbarian'
