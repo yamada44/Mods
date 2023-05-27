08aee1a9bb7225fe3381dc71eeb0c07a90f11ad7
larenz jackson <romanlegion15@gmail.com>
2023-05-26 19:06:52 -0500

    Update Client_PresentCommercePurchaseUI.lua

--- Character-Pack-Antiquity/Client_PresentCommercePurchaseUI.lua
+++ Character-Pack-Antiquity/Client_PresentCommercePurchaseUI.lua
@@ -36,7 +36,7 @@ function Client_PresentCommercePurchaseUI(rootParent, game, close)
 	if (Playerdata.Unitdata[i].Maxunits == 0) then goto next end
 
 
-	UI.CreateLabel(vert).SetText('Name: ' ..Playerdata.Unitdata[i].Name .."\nAttack Power: " .. Playerdata.Unitdata[i].unitpower .. "\nDefense Power: " .. defend .. '\nCost: ' ..  Playerdata.Unitdata[i].unitcost .. "\nMax at once: " .. Playerdata.Unitdata[i].Maxunits.. '\nFor more details on this unit type, check out the full Settings');
+	UI.CreateLabel(vert).SetText('Name: ' ..Playerdata.Unitdata[i].Name .."\nAttack Power: " .. Playerdata.Unitdata[i].unitpower .. "\nDefense Power: " .. defend .. '\nCost: ' ..  Playerdata.Unitdata[i].unitcost .. "\nMax at once: " .. Playerdata.Unitdata[i].Maxunits.. '\nMore details on this unit type in full Settings');
 	Chartracker[i] = UI.CreateTextInputField(vert).SetPlaceholderText(" Name of Character                       ").SetFlexibleWidth(1).SetCharacterLimit(30)
 	UI.CreateButton(vert).SetText(buttonmessage).SetOnClick(function () PurchaseClicked(i) end).SetInteractable(turnactive)
 
