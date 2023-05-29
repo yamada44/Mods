6840cc617c23e227c69d73cf29d1488cae1bc6bd
larenz jackson <romanlegion15@gmail.com>
2023-05-29 12:27:26 -0500

    Update Server_AdvanceTurn.lua

--- Terrain-Types/Server_AdvanceTurn.lua
+++ Terrain-Types/Server_AdvanceTurn.lua
@@ -24,7 +24,9 @@ function Server_AdvanceTurn_Start(game, addNewOrder)
                        local mod = WL.TerritoryModification.Create(ts.ID)
                         mod.SetOwnerOpt  = 0
                         mod.SetArmiesTo = 0	
-                        mod.SetStructuresOpt  = WL.StructureType.City(0)
+                        local structure = {}
+                        structure = ts.Structures[WL.StructureType.City] - ts.Structures[WL.StructureType.City]
+                        mod.SetStructuresOpt  = structure
                         local UnitdiedMessage = ''
 
                         addNewOrder(WL.GameOrderEvent.Create(0, 'Terrain type found. Changing', {}, {mod}));
