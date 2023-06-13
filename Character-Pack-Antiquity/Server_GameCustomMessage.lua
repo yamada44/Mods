a4653b708c7fbaa168c70ae36934b82dd6f04339
larenz jackson <romanlegion15@gmail.com>
2023-06-13 05:16:39 -0500

    Update Server_GameCustomMessage.lua

--- Character-Pack-Antiquity/Server_GameCustomMessage.lua
+++ Character-Pack-Antiquity/Server_GameCustomMessage.lua
@@ -4,10 +4,10 @@
 
 function Server_GameCustomMessage(game, playerID, payload, setReturnTable)
 
-   --playerdata = Mod.PlayerGameData
-    --playerdata[playerID].readrules = true
+   playerdata = Mod.PlayerGameData
+    playerdata[playerID].readrules = true
 
-    --Mod.PlayerGameData = playerdata
+    Mod.PlayerGameData = playerdata
 
     setReturnTable({});
 
