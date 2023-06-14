cd3ae9b2fc6be4a917a26d20498d2f8d0e1437a5
larenz jackson <romanlegion15@gmail.com>
2023-06-14 04:36:32 -0500

    Update Server_GameCustomMessage.lua

--- Character-Pack-Antiquity/Server_GameCustomMessage.lua
+++ Character-Pack-Antiquity/Server_GameCustomMessage.lua
@@ -5,7 +5,8 @@
 function Server_GameCustomMessage(game, playerID, payload, setReturnTable)
 local type = payload.type
   local playerdata = Mod.PlayerGameData
-    playerdata[playerID] = {type = {readrules = true}}
+    if playerdata[playerID][type] == nil then playerdata[playerID][type] = {} end
+      playerdata[playerID][type] = {readrules = true}
 
     Mod.PlayerGameData = playerdata
 
