6d7bccd057565334e809df5107d3ed9b1c1fd5d5
larenz jackson <romanlegion15@gmail.com>
2023-07-07 14:00:05 -0500

    Update Client_SaveConfigureUI.lua

--- Character-Pack-Heros/Client_SaveConfigureUI.lua
+++ Character-Pack-Heros/Client_SaveConfigureUI.lua
@@ -15,7 +15,7 @@ function Client_SaveConfigureUI(alert)
 
    for i = 1, UnitTypeMax do
         print (i)
-        if InputFieldTable[i].template.GetIsChecked() == nil or InputFieldTable[i].template.GetIsChecked() == nil then alert("Did not initialize Template ".. i) end
+        if InputFieldTable[i].template.GetIsChecked() == false or InputFieldTable[i].template == nil then alert("Did not initialize Template ".. i) end
 
         local cost = InputFieldTable[i].costInputField.GetValue();
         if cost < 1 then alert("Mod set up failed\nCost to buy this Unit must be positive\nReset to default settings"); 
