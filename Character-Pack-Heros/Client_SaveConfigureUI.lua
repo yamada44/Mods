b73c9e5b605098cd558286b7ff866f1ff2d95535
larenz jackson <romanlegion15@gmail.com>
2023-07-07 22:42:07 -0500

    Update Client_SaveConfigureUI.lua

--- Character-Pack-Heros/Client_SaveConfigureUI.lua
+++ Character-Pack-Heros/Client_SaveConfigureUI.lua
@@ -15,7 +15,7 @@ function Client_SaveConfigureUI(alert)
 
    for i = 1, UnitTypeMax do
         print (i)
-        if InputFieldTable[i].Template.GetIsChecked() == nil or InputFieldTable[i].Template == nil then alert("Did not initialize Template ".. i) 
+        if InputFieldTable[i].Template == nil or InputFieldTable[i].Template.GetIsChecked() == nil then alert("Did not initialize Template ".. i) 
             
    else 
     InputFieldTable[i].Template.SetIsChecked(false) -- when you reopen the mod menu, it will save your data and remember you already opened it
