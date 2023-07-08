62df4f2714d6604f28620b16dc4973395e8edd93
larenz jackson <romanlegion15@gmail.com>
2023-07-08 16:57:57 -0500

    Update Client_SaveConfigureUI.lua

--- Character-Pack-Heros/Client_SaveConfigureUI.lua
+++ Character-Pack-Heros/Client_SaveConfigureUI.lua
@@ -58,13 +58,13 @@ print (maxunits, 'maxunits')
         if (InputFieldTable[i].Name ~= '' and InputFieldTable[i].Name ~= nil)then
         local name = InputFieldTable[i].Name
         Mod.Settings.Unitdata[i].Name = name;
-                elseif(maxunits ~= 0)then
+        elseif(maxunits ~= 0)then
          Mod.Settings.Unitdata[i].Name = " (( NO NAME GIVEN ))" 
             UI.Alert("NO NAME GIVEN TO UNIT TYPE(S) that are enabled\nTo disable Unit Types set their Maxunit's to 0\nReset to default settings")
         end
         
 
-        print(name,'name')
+        print(InputFieldTable[i].Name,'name')
 
 
         local shared = InputFieldTable[i].Shared
