5b8a31f832d23b3f5adbc814c6158d4f98010c07
larenz jackson <romanlegion15@gmail.com>
2023-07-09 17:12:27 -0500

    Update Client_SaveConfigureUI.lua

--- Character-Pack-Heros/Client_SaveConfigureUI.lua
+++ Character-Pack-Heros/Client_SaveConfigureUI.lua
@@ -32,93 +32,93 @@ function Client_SaveConfigureUI(alert)
         Mod.Settings.Unitdata[i].unitcost = cost; end 
 
 
-        local power = InputFieldTable[i].unitpower.GetValue()
+        local power = TableFormat(InputFieldTable[i].unitpower ,num)
         if power < 0 then alert("Mod set up failed\n Units cannot have a Minimum attack Range below 0\nReset to default settings")
             Mod.Settings.Unitdata[i].unitpower = 1
         else Mod.Settings.Unitdata[i].unitpower = power; end
 
-        local maxatt = InputFieldTable[i].AttackMax.GetValue()
+        local maxatt = TableFormat(InputFieldTable[i].AttackMax ,num)
         if maxatt < 0 or maxatt < power then alert("Mod set up failed\nUnits cannot have a Maximum attack power below 0 or below Minimum attack Range\nReset to default settings"); 
             Mod.Settings.Unitdata[i].AttackMax = Mod.Settings.Unitdata[i].unitpower
         else Mod.Settings.Unitdata[i].AttackMax = maxatt; end
     
-        local maxunits = InputFieldTable[i].Maxunits.GetValue()
+        local maxunits = TableFormat(InputFieldTable[i].Maxunits ,num)
         if maxunits > 5 or maxunits < 1 then alert("Mod set up failed\nMax Units cannot be greater then 5\nReset to default settings"); 
             Mod.Settings.Unitdata[i].Maxunits = 1
         else
         Mod.Settings.Unitdata[i].Maxunits = maxunits; end
 
-        local image = InputFieldTable[i].image.GetValue()
+        local image = TableFormat(InputFieldTable[i].image ,num)
         if image < 0 or image > Maxpictures then alert("Mod set up failed\nOnly images between 0 -".. Maxpictures..'\nReset to default settings'); 
             Mod.Settings.Unitdata[i].image = 1
         else
         Mod.Settings.Unitdata[i].image = image; end
 
-        if (InputFieldTable[i].Name.GetText() ~= '' and InputFieldTable[i].Name.GetText() ~= nil)then
-        local name = InputFieldTable[i].Name.GetText()
+        if (TableFormat(InputFieldTable[i].Name,tex) ~= '' and TableFormat(InputFieldTable[i].Name,tex) ~= nil)then
+        local name = TableFormat(InputFieldTable[i].Name,tex)
         Mod.Settings.Unitdata[i].Name = name;
         elseif(maxunits ~= 0)then
          Mod.Settings.Unitdata[i].Name = " (( NO NAME GIVEN ))" 
             UI.Alert("NO NAME GIVEN TO UNIT TYPE(S) that are enabled\nTo disable Unit Types set their Maxunit's to 0\nReset to default settings")
         end
 
-        local shared = InputFieldTable[i].Shared.GetIsChecked()
+        local shared = TableFormat(InputFieldTable[i].Shared,boo)
         Mod.Settings.Unitdata[i].Shared = shared
 
-        local visible = InputFieldTable[i].Visible.GetIsChecked()
+        local visible = TableFormat(InputFieldTable[i].Visible,boo)
         Mod.Settings.Unitdata[i].Visible = visible
         
-        local maxserver = InputFieldTable[i].MaxServer.GetValue()
+        local maxserver = TableFormat(InputFieldTable[i].MaxServer ,num)
         if (maxserver > 50 or maxserver < 0 )then maxserver = 0 alert("Max amount over entire server input outside data range. min: 0, Max: 50")
         else Mod.Settings.Unitdata[i].MaxServer = maxserver end
 
  
-        local minlife = InputFieldTable[i].Minlife.GetValue()
+        local minlife = TableFormat(InputFieldTable[i].Minlife ,num)
        if minlife < 0 or minlife > 99 then 
         alert('Minimum amount for life is 0\nReset to default settings')
         Mod.Settings.Unitdata[i].Minlife = 0
        else Mod.Settings.Unitdata[i].Minlife = minlife end
 
-       local maxlife = InputFieldTable[i].Maxlife.GetValue()
+       local maxlife = TableFormat(InputFieldTable[i].Maxlife ,num)
        if maxlife < minlife or maxlife > 100 then 
         alert('Minimum amount for Max life is equal to "Minimum Turns alive" setting\n Max amount is 100 \nReset to default settings')
         Mod.Settings.Unitdata[i].Maxlife = Mod.Settings.Unitdata[i].Minlife
        else Mod.Settings.Unitdata[i].Maxlife = maxlife end
 
        --transfer
-       local transfer = InputFieldTable[i].Transfer.GetValue()
+       local transfer = TableFormat(InputFieldTable[i].Transfer ,num)
        if (transfer > 25 or transfer < -1 )then transfer = 0 alert("Transfer input outside data range. min: 0, Max: 25")
        else Mod.Settings.Unitdata[i].Transfer = transfer end
 
        --leveling
-       local level = InputFieldTable[i].Level.GetValue()
+       local level = TableFormat(InputFieldTable[i].Level ,num)
        if (level > 5000 or level < 0 )then level = 0 alert("level input outside data range. min: 0, Max: 5000")
        else Mod.Settings.Unitdata[i].Level = level end
 
        --Active
-       local active = InputFieldTable[i].Active.GetValue()
+       local active = TableFormat(InputFieldTable[i].Active ,num)
        if (active > 200 or active < 0 )then active = 0 alert("unit Active input outside data range. min: 0, Max: 200")
        else Mod.Settings.Unitdata[i].Active = active end
 
        --defence
-       local defend = InputFieldTable[i].Defend.GetValue()
+       local defend = TableFormat(InputFieldTable[i].Defend ,num)
        if (defend < 0 )then defend = 0 alert("Mod set up failed\n Units cannot have a defence power below 0\nReset to default settings")
        else Mod.Settings.Unitdata[i].Defend = defend end
 
-       local altmoves = InputFieldTable[i].Altmoves.GetIsChecked()
+       local altmoves = TableFormat(InputFieldTable[i].Altmoves,boo)
        Mod.Settings.Unitdata[i].Altmoves = altmoves
 
        --cooldown
-       local cooldown = InputFieldTable[i].Cooldown.GetValue()
+       local cooldown = TableFormat(InputFieldTable[i].Cooldown ,num)
        if (cooldown < 0 or cooldown > 100)then cooldown = 0 alert("Mod set up failed\n Units cannot have a cool down timer longer then 100 or below 0\n(set to 0 to disable)")
        else Mod.Settings.Unitdata[i].Cooldown = cooldown end
 
        --host Rules
-        local hostrules = InputFieldTable[i].HostRules.GetText()
+        local hostrules = TableFormat(InputFieldTable[i].HostRules,tex)
         Mod.Settings.Unitdata[i].HostRules = hostrules;
 
         --Assassnation
-        local assass = InputFieldTable[i].Assassination.GetValue()
+        local assass = TableFormat(InputFieldTable[i].Assassination ,num)
         if (assass < 0 or assass > 5)then assass = 0 alert("Mod set up failed\n Assassination level must be between 0 and 5\n(set to 0 to disable)")
         else Mod.Settings.Unitdata[i].Assassination = assass end
 
