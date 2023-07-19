d6dc92d67ec0ba8330653adf784274dbd519a91b
larenz jackson <romanlegion15@gmail.com>
2023-07-19 13:14:47 -0500

    Update Server_StartGame.lua

--- Character-Pack-Antiquity/Server_StartGame.lua
+++ Character-Pack-Antiquity/Server_StartGame.lua
@@ -3,7 +3,7 @@ function Server_StartGame (game,standing)
 
      unitdata = Mod.Settings.Unitdata
      local typeamount = Mod.Settings.BeforeMax
-
+if false then
      -- loop through all territories, find value and add special unit type
      for i=1, typeamount do
         for _,ts in pairs(game.ServerGame.LatestTurnStanding.Territories) do
@@ -12,4 +12,5 @@ function Server_StartGame (game,standing)
             end
         end
     end
+end
 end
