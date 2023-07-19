6a389bf337d7145ed31ab55f6e6abf204f8e5525
larenz jackson <romanlegion15@gmail.com>
2023-07-19 13:19:48 -0500

    Update Server_StartGame.lua

--- Character-Pack-Antiquity/Server_StartGame.lua
+++ Character-Pack-Antiquity/Server_StartGame.lua
@@ -7,12 +7,12 @@ function Server_StartGame (game,standing)
      -- loop through all territories, find value and add special unit type
      for i=1, typeamount do
 
-        for _,ts in pairs(game.ServerGame.LatestTurnStanding.Territories) do
-            if false then
+        for _,ts in pairs(standing.Territories) do
+  --          if false then
             if ts.NumArmies.NumArmies == unitdata[i].Autovalue then
-            --   standing.NumArmies.NumArmies = 9999 
+               standing.NumArmies.NumArmies = 9999 
             end
-        end
+      --  end
     end
 end
 end
