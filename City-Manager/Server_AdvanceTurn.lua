b1b32b22532a16e5da4b4c5faa2c8ff5bff30446
larenz jackson <romanlegion15@gmail.com>
2023-07-18 20:34:28 -0500

    kol

---
+++ City-Manager/Server_AdvanceTurn.lua
@@ -0,0 +1,48 @@
+require('Utilities')
+
+
+function Server_AdvanceTurn_End(game, addNewOrder)
+    local publicdata = Mod.PublicGameData
+local cityGroups = publicdata.cityGroups
+print (cityGroups )
+if cityGroups == nil then cityGroups = {} end
+print (cityGroups )
+
+    for _, bonus in pairs(game.Map.Bonuses) do
+        print('bonus ------ ', bonus.Name)
+        if cityGroups[bonus.ID] == nil then
+            cityGroups[bonus.ID] = {} 
+            cityGroups[bonus.ID].Hascity = false
+        end 
+
+        if game.Settings.OverriddenBonuses[bonus.ID] ~= 0 then
+            for _, t in pairs(bonus.Territories) do
+
+                local terr = game.ServerGame.LatestTurnStanding.Territories[t]
+                local Cities = terr.Structures;
+                if (Cities == nil) then Cities = {}; end;
+
+                if(terr.Structures ~= nil)then
+                print (terr.Structures[WL.StructureType.City])
+                end
+                
+                if cityGroups[bonus.ID].Hascity == true and cityGroups[bonus.ID].citylocation ~= t and Cities[WL.StructureType.City] ~= nil and Cities[WL.StructureType.City] > 0 then
+                    Cities[WL.StructureType.City] = 0
+
+                    local mod = WL.TerritoryModification.Create(t)
+                    mod.SetStructuresOpt = Cities
+                    local UnitdiedMessage = ''
+                    addNewOrder(WL.GameOrderEvent.Create(0, 'Illegal city. Only 1 city stack per bonus', {}, {mod}));
+                elseif cityGroups[bonus.ID].Hascity == false and Cities[WL.StructureType.City] ~= nil and Cities[WL.StructureType.City] > 0 then
+                    cityGroups[bonus.ID].Hascity = true
+                    cityGroups[bonus.ID].citylocation = t
+
+                end
+
+            end 
+        end
+    end
+
+    publicdata.cityGroups = cityGroups
+Mod.PublicGameData = publicdata
+end
