e8b967d14f571feecfa7982bbf509929f6ff784b
larenz jackson <romanlegion15@gmail.com>
2023-07-23 15:12:26 -0500

    Update Server_AdvanceTurn.lua

--- Character-Pack-World-Wars/Server_AdvanceTurn.lua
+++ Character-Pack-World-Wars/Server_AdvanceTurn.lua
@@ -313,7 +313,7 @@ function Deathlogic(game, order, result, skipThisOrder, addNewOrder)
 
 						local terrMod = WL.TerritoryModification.Create(order.To);
 						terrMod.AddSpecialUnits = {builder.Build()};
-						addNewOrder(WL.GameOrderEvent.Create(land.OwnerPlayerID, transfermessage, nil, {terrMod}));
+						addNewOrder(WL.GameOrderEvent.Create(landfrom.OwnerPlayerID, transfermessage, nil, {terrMod}));
 
 					else
 						addNewOrder(WL.GameOrderEvent.Create(land.OwnerPlayerID , UnitKilledMessage , nil,nil,nil ,{} ))
