d3e40bc4ea1840b9ae1d9c718f7174bd9101d58f
larenz jackson <romanlegion15@gmail.com>
2023-07-23 12:06:06 -0500

    Update Server_AdvanceTurn.lua

--- Character-Pack-World-Wars/Server_AdvanceTurn.lua
+++ Character-Pack-World-Wars/Server_AdvanceTurn.lua
@@ -370,7 +370,7 @@ print (altmove,'altmove')
 									terrMod.RemoveSpecialUnitsOpt = {v.ID}
 
 									local skipmessage = 'Moved order for this unit was skipped because its not an even turn'
-									addNewOrder(WL.GameOrderEvent.Create(order.PlayerID, skipmessage , nil, nil, nil, {terrMod}));
+									addNewOrder(WL.GameOrderEvent.Create(order.PlayerID, skipmessage , nil, {terrMod}));
 									
 
 								--skipThisOrder(WL.ModOrderControl.SkipAndSupressSkippedMessage); 
