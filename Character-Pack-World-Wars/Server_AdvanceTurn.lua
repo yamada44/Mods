549df9d5b4df0c246669c5ce485da79697eb5b0f
larenz jackson <romanlegion15@gmail.com>
2023-07-23 12:43:53 -0500

    Update Server_AdvanceTurn.lua

--- Character-Pack-World-Wars/Server_AdvanceTurn.lua
+++ Character-Pack-World-Wars/Server_AdvanceTurn.lua
@@ -364,11 +364,11 @@ print (altmove,'altmove')
 
 									local builder = WL.CustomSpecialUnitBuilder.CreateCopy(v);
 
-									local terrMod = WL.TerritoryModification.Create(order.To); -- adding it to territory logic
+									local terrMod = WL.TerritoryModification.Create(order.From); -- adding it to territory logic
 									local terrNomove = WL.TerritoryModification.Create(order.From); -- adding it to territory logic
 
-									terrMod.AddSpecialUnits = {builder.Build()};
-									terrNomove.RemoveSpecialUnitsOpt = {v.ID}
+									terrMod.AddSpecialUnits = {v.ID};
+									terrNomove.RemoveSpecialUnitsOpt = {builder.Build()}
 
 									local skipmessage = 'Moved order for this unit was skipped because its not an even turn'
 									addNewOrder(WL.GameOrderEvent.Create(order.PlayerID, skipmessage , nil, {terrMod}));
