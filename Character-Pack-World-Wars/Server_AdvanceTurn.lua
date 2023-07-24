4feccac79c5c8988688f5a5b661626bc6d2ea5ed
larenz jackson <romanlegion15@gmail.com>
2023-07-24 03:26:56 -0500

    Update Server_AdvanceTurn.lua

--- Character-Pack-World-Wars/Server_AdvanceTurn.lua
+++ Character-Pack-World-Wars/Server_AdvanceTurn.lua
@@ -452,7 +452,7 @@ print (altmove,'altmove')
 			addNewOrder(WL.GameOrderAttackTransfer.Create(order.PlayerID,order.From,order.To,1,false,Game2.ServerGame.LatestTurnStanding.Territories[order.From].NumArmies,false))
 			addNewOrder(WL.GameOrderEvent.Create(order.PlayerID, 'territory Mod' , {}, {NoMterrMod}))
 
-		--	skipThisOrder(WL.ModOrderControl.SkipAndSupressSkippedMessage)
+			skipThisOrder(WL.ModOrderControl.SkipAndSupressSkippedMessage)
 			end
 
 
