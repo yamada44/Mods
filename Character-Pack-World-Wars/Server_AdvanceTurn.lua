2303fff99158b03e10745c4f2d8d290d1bb88fc4
larenz jackson <romanlegion15@gmail.com>
2023-07-25 03:27:41 -0500

    Update Server_AdvanceTurn.lua

--- Character-Pack-World-Wars/Server_AdvanceTurn.lua
+++ Character-Pack-World-Wars/Server_AdvanceTurn.lua
@@ -452,10 +452,10 @@ print (altmove,'altmove')
 			NoMterrMod.AddSpecialUnits = buildertalble;
 
 			addNewOrder(WL.GameOrderEvent.Create(order.PlayerID, skipmessage , {}, {NoMterrNomove}))-- remove from territory
-			addNewOrder(WL.GameOrderAttackTransfer.Create(order.PlayerID,order.From,order.To,1,false,Game2.ServerGame.LatestTurnStanding.Territories[order.From].NumArmies,false))
+			addNewOrder(WL.GameOrderAttackTransfer.Create(order.PlayerID,order.From,order.To,3,false,Game2.ServerGame.LatestTurnStanding.Territories[order.From].NumArmies,false))
 			addNewOrder(WL.GameOrderEvent.Create(order.PlayerID, 'territory Mod' , {}, {NoMterrMod}))
 
-			--skipThisOrder(WL.ModOrderControl.SkipAndSupressSkippedMessage)
+			skipThisOrder(WL.ModOrderControl.SkipAndSupressSkippedMessage)
 			
 			end
 
@@ -554,3 +554,57 @@ print (altmove,'altmove')
 		
 	end
 
+
+
+	function Server_AdvanceTurn_End(game, addNewOrder)
+		local publicdata = Mod.PublicGameData
+	local cityGroups = publicdata.cityGroups
+	print (cityGroups )
+	if cityGroups == nil then cityGroups = {} end
+	
+	
+	
+		for _, bonus in pairs(game.Map.Bonuses) do
+			print('bonus ------ ', bonus.Name)
+			if cityGroups[bonus.ID] == nil then
+				cityGroups[bonus.ID] = {} 
+				cityGroups[bonus.ID].Hascity = false
+			end 
+	
+			if game.Settings.OverriddenBonuses[bonus.ID] ~= 0 then
+				for _, t in pairs(bonus.Territories) do
+	
+					local terr = game.ServerGame.LatestTurnStanding.Territories[t]
+					local Cities = terr.Structures;
+					if (Cities == nil) then Cities = {}; end;
+	
+					if(terr.Structures ~= nil)then
+					print (terr.Structures[WL.StructureType.City])
+					end
+					
+					if cityGroups[bonus.ID].Hascity == true and cityGroups[bonus.ID].citylocation ~= t and Cities[WL.StructureType.City] ~= nil and Cities[WL.StructureType.City] > 0 then
+						Cities[WL.StructureType.City] = 0
+	
+						local mod = WL.TerritoryModification.Create(t)
+						mod.SetStructuresOpt = Cities
+						local UnitdiedMessage = ''
+						addNewOrder(WL.GameOrderEvent.Create(terr.OwnerPlayerID , 'Illegal city placed. Removing\nOnly 1 city stack per bonus', {}, {mod}));
+					elseif cityGroups[bonus.ID].Hascity == false and Cities[WL.StructureType.City] ~= nil and Cities[WL.StructureType.City] > 0 then
+						cityGroups[bonus.ID].Hascity = true
+						cityGroups[bonus.ID].citylocation = t
+	
+					end
+	
+				end 
+			end
+		end
+	
+	  --game.Settings.CustomScenario.SlotsAvailable
+			
+	
+		publicdata.cityGroups = cityGroups
+	Mod.PublicGameData = publicdata
+	end
+	
+	
+	
