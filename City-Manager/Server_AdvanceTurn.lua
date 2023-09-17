2b9d2e1e5aae72f68e44779114fb2fab3231dae3
larenz jackson <romanlegion15@gmail.com>
2023-09-17 04:00:09 -0500

    Update Server_AdvanceTurn.lua

--- City-Manager/Server_AdvanceTurn.lua
+++ City-Manager/Server_AdvanceTurn.lua
@@ -15,6 +15,8 @@ if cityGroups == nil then cityGroups = {} end
             cityGroups[bonus.ID] = {} 
             cityGroups[bonus.ID].Hascity = false
         end 
+        print (game.Settings.OverriddenBonuses[bonus.ID])
+        print   (bonus.Amount)
 local Noskip = Bonuschecker(game.Settings.OverriddenBonuses[bonus.ID], bonus.Amount)
         if Noskip == true  then
 print ('did you print value')
