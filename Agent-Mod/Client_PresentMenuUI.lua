02e9fe85f333e458cf7446d374d3366b1e70caad
larenz jackson <romanlegion15@gmail.com>
2023-11-21 01:12:34 -0600

    Update Client_PresentMenuUI.lua

--- Agent-Mod/Client_PresentMenuUI.lua
+++ Agent-Mod/Client_PresentMenuUI.lua
@@ -401,7 +401,7 @@ function CreateOrder(type,close, data)
 	baseload.text = Findmatch(publicdata[ID].Agency.Agentlist,Agentdata,"agentID")
 	baseload.cost = MissionCost
 
-	--local killagent_Index = Findmatch(publicdata[ID].Agency.Agentlist,data,"agentID")
+	local killagent_Index = Findmatch(Agentlist,data.agentid,"agentID")
 	local agentsent = Agentdata -- just to clear up the name
 	local datasent2 = 0
 	if Targettype ~= "KillAgent" and Targettype ~= "Killcard" and Game.LatestStanding.Territories[SelectedTerritory.ID].FogLevel > 3 then UI.Alert("Must pick a territory with visible Territory") return end
@@ -419,14 +419,13 @@ function CreateOrder(type,close, data)
 			datasent = Cardsource
 		elseif type == 2 then -- getting agent on agent action
 			targetagentplayerID = data.playeridofagent
-			indexofagent = data.agentid
 			print(targetagentplayerID)
-			print(indexofagent)
+			print(killagent_Index)
 			print(publicdata[ID].Agency.Agentlist[baseload.text].codename)
-			print(publicdata[targetagentplayerID].Agency.Agentlist[indexofagent].codename)
-			msg = "Agent " .. publicdata[ID].Agency.Agentlist[baseload.text].codename .. " has began a assassination operation on agent " .. publicdata[targetagentplayerID].Agency.Agentlist[indexofagent].codename
+			print(publicdata[targetagentplayerID].Agency.Agentlist[killagent_Index].codename)
+			msg = "Agent " .. publicdata[ID].Agency.Agentlist[baseload.text].codename .. " has began a assassination operation on agent " .. publicdata[targetagentplayerID].Agency.Agentlist[killagent_Index].codename
 			datasent = data
-			datasent2 = publicdata[targetagentplayerID].Agency.Agentlist[indexofagent].PlayerofAgentID
+			datasent2 = publicdata[targetagentplayerID].Agency.Agentlist[killagent_Index].PlayerofAgentID
 		end
 		Orderstartwith = Targettype
 		local SelectedTerritoryID 
