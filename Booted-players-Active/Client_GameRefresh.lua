d06c1b9d6a5452cf223ec26e341fd4a638e0bbb6
larenz jackson <romanlegion15@gmail.com>
2023-12-15 10:31:19 -0600

    Update Client_GameRefresh.lua

--- Booted-players-Active/Client_GameRefresh.lua
+++ Booted-players-Active/Client_GameRefresh.lua
@@ -3,6 +3,7 @@
 function Client_GameRefresh(game)
     publicdata = Mod.PublicGameData
 	local voteTimer = 3
+	Game = game
 
 	if (not Alerted and not WL.IsVersionOrHigher or not WL.IsVersionOrHigher("5.21")) then
 		UI.Alert("You must update your app to the latest version to use the Player Manager mod");
