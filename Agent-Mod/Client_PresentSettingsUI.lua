9947e9b73ea651ea820bf75542a7ee3e29cbe771
larenz jackson <romanlegion15@gmail.com>
2023-12-18 10:47:34 -0600

    Update Client_PresentSettingsUI.lua

--- Agent-Mod/Client_PresentSettingsUI.lua
+++ Agent-Mod/Client_PresentSettingsUI.lua
@@ -10,17 +10,17 @@ function Client_PresentSettingsUI(rootParent)
 	local Citylost = Mod.Settings.Citylost
 	local Cardsremoved = Mod.Settings.Cardsremoved
 	local MissionCost = Mod.Settings.MissionCost
-
+if Cardsremoved == true then Cardsremoved = "true" else Cardsremoved = "false"
 
 	local vert = UI.CreateVerticalLayoutGroup(rootParent)
 
-	UI.CreateLabel(vert).SetText('Gold needed to create a spy agency: $' .. creationfee).SetColor('#F3FFAE')
-	UI.CreateLabel(vert).SetText('Agent/spy cost: $' .. Agentcost).SetColor('#F3FFAE')
-    UI.CreateLabel(vert).SetText('Decoy Agent cost: $' .. Decoycost).SetColor('#F3FFAE')
+	UI.CreateLabel(vert).SetText('Gold needed to create a spy agency: ' .. creationfee).SetColor('#F3FFAE')
+    UI.CreateLabel(vert).SetText('operation cost to launch a mission: ' .. MissionCost).SetColor('#F3FFAE')
+    UI.CreateLabel(vert).SetText('Agent/spy cost: ' .. Agentcost).SetColor('#F3FFAE')
+    UI.CreateLabel(vert).SetText('Decoy Agent cost: ' .. Decoycost).SetColor('#F3FFAE')
 	UI.CreateLabel(vert).SetText('Armies removed when a agent sabotages logistics: ' .. ArmiesLost).SetColor('#F3FFAE')
 	UI.CreateLabel(vert).SetText('cities removed when a agent sabotages logistics: ' .. Citylost).SetColor('#F3FFAE')
 	UI.CreateLabel(vert).SetText('Resting time between missions for agents (turns): ' .. Cooldown).SetColor('#F3FFAE')
 	UI.CreateLabel(vert).SetText('Can cards be removed in this game: ' .. Cardsremoved).SetColor('#F3FFAE')
-	UI.CreateLabel(vert).SetText('operation cost to launch a mission: $' .. MissionCost).SetColor('#F3FFAE')
 
 end
