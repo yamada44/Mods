9eb915ef1ef95048e29c788dbbbf5dade361be73
larenz jackson <romanlegion15@gmail.com>
2023-12-20 23:39:54 -0600

    kol

--- Booted-players-Active/Server_AdvanceTurn.lua
+++ Booted-players-Active/Server_AdvanceTurn.lua
@@ -2,6 +2,25 @@ require('Utilities')
 
 
 function Server_AdvanceTurn_Start(game, addNewOrder)
+-- only for 1 or two turns
+    local goldhave
+    local MaxGold = 19
+    local added = 100
+    local standing = game.ServerGame.LatestTurnStanding
+    for playerID, player in pairs(game.Game.PlayingPlayers) do
+        if (not player.IsAIOrHumanTurnedIntoAI) then 
+           -- if playergroup[playerID] == nil then playergroup[playerID] = {} end
+            local income = player.Income(0, standing, true, true) 
+            goldhave = game.ServerGame.LatestTurnStanding.NumResources(playerID, WL.ResourceType.Gold)
+           if income.Total <= MaxGold then
+            local incomeMod = WL.IncomeMod.Create(playerID, added, 'Income for being weak')
+            addNewOrder(WL.GameOrderEvent.Create(playerID, "Added income " , nil, {},nil,{incomeMod}));
+            --game.ServerGame.SetPlayerResource(playerID, WL.ResourceType.Gold, goldhave + 100)
+            --
+           end
+        end
+    end
+
 
   publicdata = Mod.PublicGameData
   InActionAlready = {}
--- tools/Server_AdvanceTurn.lua
+++ tools/Server_AdvanceTurn.lua
@@ -3,22 +3,25 @@ require('Utilities')
 
 function Server_AdvanceTurn_End(game, addNewOrder)
     local publicdata = Mod.PublicGameData
-    local playergroup = {}
+    --local playergroup = {}
     local goldhave
-    local MaxGold = 20
+    local MaxGold = 19
+    local added = 100
+    local standing = game.ServerGame.LatestTurnStanding
     for playerID, player in pairs(game.Game.PlayingPlayers) do
         if (not player.IsAIOrHumanTurnedIntoAI) then 
-            if playergroup[playerID] == nil then playergroup[playerID] = {} end
-          --  player.Income(0, standing GameStanding, true, true) 
+           -- if playergroup[playerID] == nil then playergroup[playerID] = {} end
+            local income = player.Income(0, standing, true, true) 
             goldhave = game.ServerGame.LatestTurnStanding.NumResources(playerID, WL.ResourceType.Gold)
-           if goldhave <= MaxGold then
-            game.ServerGame.SetPlayerResource(playerID, WL.ResourceType.Gold, goldhave - typecost)
+           if income.Total <= MaxGold then
+            local incomeMod = WL.IncomeMod.Create(playerID, added, 'Income for being weak')
+            addNewOrder(WL.GameOrderEvent.Create(playerID, "Added income " , nil, {},nil,{incomeMod}));
+            --game.ServerGame.SetPlayerResource(playerID, WL.ResourceType.Gold, goldhave + 100)
+            --
            end
         end
     end
-    for playerID, player in pairs(game.Game.PlayingPlayers) do 
-        if playergroup[playerID].GoldperTurn >
-    end
+
   --game.Settings.CustomScenario.SlotsAvailable
         
 
