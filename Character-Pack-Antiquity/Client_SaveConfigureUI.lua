9149a1b7b60065ed46b67bcc1ced99557dd613e3
larenz jackson <romanlegion15@gmail.com>
2024-03-22 21:36:09 -0500

    Update Client_SaveConfigureUI.lua

--- Character-Pack-Antiquity/Client_SaveConfigureUI.lua
+++ Character-Pack-Antiquity/Client_SaveConfigureUI.lua
@@ -153,25 +153,26 @@ function Client_SaveConfigureUI(alert)
          noUnitsOn = noUnitsOn + maxunits -- to check if any units were turned on
 
     --Slot Logic
-    local slottex = TableFormat(InputFieldTable[i].Slot.GetText(),tex)
-    local slotamount = split(slottex, '/')
-    local slotnumbers = {}
-    for i,v in pairs (slotamount) do
-      if string.match(v, '%D+') then
-       alert("Mod set up failed\nCan only inlcude numbers for slot days") 
-      return
-      elseif string.len(v) > 2 or string.len(v) < 1 then
-       alert("Mod set up failed\nEach Slot must remain between 1-99 ") 
-       return
-      end
-      table.insert(slotnumbers,tonumber(v - 1))
-      
-     end
-    
-    --Slot
-    
-    Mod.Settings.Unitdata[i].Slot = slotnumbers
-    Mod.Settings.Unitdata[i].Slotstore = slottex
+    if InputFieldTable[i].Slot ~= nil then
+        local slottex = TableFormat(InputFieldTable[i].Slot.GetText(),tex)
+        local slotamount = split(slottex, '/')
+        local slotnumbers = {}
+        for i,v in pairs (slotamount) do
+            if string.match(v, '%D+') then
+                alert("Mod set up failed\nCan only inlcude numbers for slot days") 
+                return
+            elseif string.len(v) > 2 or string.len(v) < 1 then
+                alert("Mod set up failed\nEach Slot must remain between 1-99 ") 
+                return
+            end
+            table.insert(slotnumbers,tonumber(v - 1))
+        
+        end
+        
+        --Slot
+        Mod.Settings.Unitdata[i].Slot = slotnumbers
+        Mod.Settings.Unitdata[i].Slotstore = slottex
+    end
 
     end
     Mod.Settings.Unitdata[i].TemplateStored = InputFieldTable[i].TemplateStored -- storing and saving of unit type
