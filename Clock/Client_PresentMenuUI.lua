f1989719e0165e055b4ece6d3224ded49a89eeb0
larenz jackson <romanlegion15@gmail.com>
2024-04-30 12:11:38 -0500

    8

--- Clock/Client_PresentMenuUI.lua
+++ Clock/Client_PresentMenuUI.lua
@@ -63,7 +63,9 @@ function ShowLayout(vert2,rootParent,setMaxSize)
 	local ViewableTable = oldtableNewformat(short[ID].template.Display,"value",false)
 	local ViewableTable2 = oldtableNewformat(short[ID].template.Display,"text",false)
 	local ViewableTable3 = oldtableNewformat(short[ID].template.Display,"",true)
-
+for i,v in pairs(ViewableTable2)do
+	print(i,v,"messed up test")
+end
 
 	local tpyename = ViewableTable --accesses the field
 	if #tpyename <= 3 then setMaxSize(680-350, 400) else setMaxSize(720, 400) end
