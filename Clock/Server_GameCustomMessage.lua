6c50f58f2bbedebaa2a1cc0296a30320cf019be0
larenz jackson <romanlegion15@gmail.com>
2024-05-02 18:01:59 -0500

    Update Server_GameCustomMessage.lua

--- Clock/Server_GameCustomMessage.lua
+++ Clock/Server_GameCustomMessage.lua
@@ -30,31 +30,13 @@ print(playerID)
 
   elseif type == 2 then --changing viewing order
 
-    Pub[playerID].template.Display[1].value = data1[1].value
-    Pub[playerID].template.Display[1].text = data1[1].text
-    Pub[playerID].template.Display[1].order = 1
-    Pub[playerID].template.Display[2].value = data1[2].value
-    Pub[playerID].template.Display[2].text = data1[2].text
-    Pub[playerID].template.Display[2].order = 2
-    Pub[playerID].template.Display[3].value = data1[3].value
-    Pub[playerID].template.Display[3].text = data1[3].text
-    Pub[playerID].template.Display[3].order = 3
-    Pub[playerID].template.Display[4].value = data1[4].value
-    Pub[playerID].template.Display[4].text = data1[4].text
-    Pub[playerID].template.Display[4].order = 4
-    Pub[playerID].template.Display[5].value = data1[5].value
-    Pub[playerID].template.Display[5].text = data1[5].text
-    Pub[playerID].template.Display[5].order = 5
-    Pub[playerID].template.Display[6].value = data1[6].value
-    Pub[playerID].template.Display[6].text = data1[6].text
-    Pub[playerID].template.Display[6].order = 6
-    Pub[playerID].template.Display[7].value = data1[7].value
-    Pub[playerID].template.Display[7].text = data1[7].text
-    Pub[playerID].template.Display[7].order = 7
-    Pub[playerID].template.Display[8].value = data1[8].value
-    Pub[playerID].template.Display[8].text = data1[8].text
-    Pub[playerID].template.Display[8].order = 8
+    for i = 1, Pub[playerID].template.Display do
+    
+      Pub[playerID].template.Display[i].value = data1[i].value
+      Pub[playerID].template.Display[i].text = data1[i].text
+      Pub[playerID].template.Display[i].order = i
 
+    end
 
   end
 
