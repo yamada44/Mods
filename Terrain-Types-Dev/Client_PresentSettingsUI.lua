94c4c248fa6508fec5ae087bb728e66ff4e29c01
larenz jackson <romanlegion15@gmail.com>
2024-05-11 20:46:31 -0500

    Update Client_PresentSettingsUI.lua

--- Terrain-Types-Dev/Client_PresentSettingsUI.lua
+++ Terrain-Types-Dev/Client_PresentSettingsUI.lua
@@ -17,47 +17,51 @@ function Stats(data, vert)
 	local row1 = UI.CreateHorizontalLayoutGroup(vert)
 	local Data = data
 
-	local Name = "<#DEF265>Terrain Name: </><#FF7AF3>" .. Data.name .. "</>\n"
+	local Name = "<#DEF265>Terrain Name: </><#FF7AF3>" .. Data.C_Name .. "</>\n"
 
 	AddStringToUI(vert,Name,nil) -- Included first cause no terrain type might be found
 
-	if Data.turnstart ~= nil then -- making sure a terrain type exist
+	AddStringToUI(vert,"<#DEF265>AutoFind number: </><#FF7AF3>" .. Data.C_Autofind .. "</>\n",nil)
+	AddStringToUI(vert,"<#DEF265>AutoFind port number: </><#FF7AF3>" .. Data.C_AutoP .. "</>\n",nil)
 
 -- Settings Display
-	local armyrules = Data.armyValueChange
+	local armyrules = Data.C_Value
 		if armyrules == -1 then armyrules = "Keep current army Value" 
 		else armyrules = "Army value changed to " .. armyrules end
-	local ownerrules = Data.OwnerID
+	local ownerrules = Data.C_TerrainTypeID
 			if ownerrules == 0 then ownerrules = "Terrain Owner is neutral"
 			elseif ownerrules == nil then ownerrules = "No ownership change"
 			else
 				ownerrules = "Manual player ID input"
 			end
-	local Modrules = Data.ModControl
+	local Modrules = Data.C_Modsetting
 			if Modrules == -1 then Modrules = "No Mods defined"
 			elseif Modrules == 0 then Modrules = "All mods defined"
 			elseif Modrules == 1 then Modrules = "Only Character pack mods defined"
 			else Modrules = " Only " .. Characterpackloader(Modrules) .. " Characters Defined" end
-	local unittype = Data.UnitControl
+	local unittype = Data.C_Unittype
 			if unittype == 0 then unittype = "Entire Character pack Defined"
 			else unittype = "The " .. unittype .. "th character unit type defined. (view settings for unit type)" end
-	local Basesettings = Data.BaseSettings
+	local Basesettings = Data.C_Inverse
 
 			if Basesettings == 1 then Basesettings = "Apply function on terrain unless Special units (defined in settings) are detected"
 			elseif Basesettings == 2 then Basesettings = "Apply function on on terrain and remove special units (defined in settings)"
 			elseif Basesettings == 3 then Basesettings = "Apply function on terrain and Remove special units (defined in settings)"
 			elseif Basesettings == 4 then Basesettings =  "Apply function on terrain except tiles with Specil units (defined in settings)" end
-	local turn = Data.turnstart
+	local turn = Data.C_Turnstart
 			if turn == 0 then turn = "Always active"
-			else turn = "The Terrain last between the turns " .. Data.turnstart .. " - " .. Data.turnend - 1 end
-
+			else turn = "The Terrain last between the turns " .. Data.C_Turnstart .. " - " .. Data.C_Turnend - 1 end
+	local build = Data.C_RemoveBuild
+			if build == true then build = "Cities/Forts/Fort Tactics builds are removed" 
+			else build = "no structures removed" end
 			AddStringToUI(vert,"<#DEF265>Army Rules: </>" .. armyrules,nil)
 			AddStringToUI(vert,"<#DEF265>Terrain ID: </>" .. ownerrules,nil)
 			AddStringToUI(vert,"<#DEF265>Mod Special units: </>".. Modrules,nil)
 			AddStringToUI(vert,"<#DEF265>Unit Type: </>" .. unittype,nil)
 			AddStringToUI(vert,"<#DEF265>Base Rules: </>" .. Basesettings,nil)
+			AddStringToUI(vert,"<#DEF265>Structure Rules: </>" .. build,nil)
 			AddStringToUI(vert,"<#DEF265>Duration: </>" .. turn,nil)
-	end
+			
 
 
 
