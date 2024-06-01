8d9cfce9b93fb30d947564f8962426dceb8a01bb
larenz jackson <romanlegion15@gmail.com>
2024-06-01 17:53:18 -0500

    Update Server_AdvanceTurn.lua

--- City-Manager/Server_AdvanceTurn.lua
+++ City-Manager/Server_AdvanceTurn.lua
@@ -25,7 +25,7 @@ if cityGroups == nil then cityGroups = {} end
 
                 local terr = game.ServerGame.LatestTurnStanding.Territories[t]
                 local Cities = terr.Structures
-                if (Cities == nil) then Cities = {}; end;
+                if (Cities == nil) then Cities = {}; end
 
                 
                 if cityGroups[bonus.ID].Hascity == true and cityGroups[bonus.ID].citylocation ~= t and Cities[WL.StructureType.City] ~= nil and Cities[WL.StructureType.City] > 0 then
@@ -37,7 +37,7 @@ if cityGroups == nil then cityGroups = {} end
                     addNewOrder(WL.GameOrderEvent.Create(terr.OwnerPlayerID , 'Illegal city placed. Removing\nOnly 1 city stack per bonus\nplace your city in '.. game.Map.Territories[cityGroups[bonus.ID].citylocation].Name , {}, {mod}))
                 elseif cityGroups[bonus.ID].Hascity == false and Cities[WL.StructureType.City] ~= nil and Cities[WL.StructureType.City] > 0 then
                     local Control = WholeControl(bonus,terr.OwnerPlayerID)
-                    print(t.OwnerPlayerID,"owner")
+
                     --Checking to see if the player controls the whole bonus if their placing their first city
                     if Control then
                         cityGroups[bonus.ID].Hascity = true
