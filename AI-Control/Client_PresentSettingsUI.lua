b47d8048ba61af7a3017d7eb937e0a984782651c
larenz jackson <romanlegion15@gmail.com>
2024-08-11 20:53:48 -0500

    Update Client_PresentSettingsUI.lua

--- AI-Control/Client_PresentSettingsUI.lua
+++ AI-Control/Client_PresentSettingsUI.lua
@@ -38,26 +38,26 @@ local row00 = UI.CreateHorizontalLayoutGroup(vert) -- Human Text
 UI.CreateLabel(row00).SetText("Human AI").SetColor('#0000FF')
 
 	local rowH9 = UI.CreateHorizontalLayoutGroup(vert) 
-	UI.CreateLabel(rowH9).SetText('Human AI can attack: ' .. tostring(M.Settings.H_attack)).SetColor('#00B5FF')
+	UI.CreateLabel(rowH9).SetText('Human AI can attack: ' .. tostring(Mod.Settings.H_attack)).SetColor('#00B5FF')
 
 	local rowH2 = UI.CreateHorizontalLayoutGroup(vert) 
-	UI.CreateLabel(rowH2).SetText('Human AI can attack: ' .. tostring(M.Settings.H_deploy)).SetColor('#00B5FF')
+	UI.CreateLabel(rowH2).SetText('Human AI can attack: ' .. tostring(Mod.Settings.H_deploy)).SetColor('#00B5FF')
 
 	local rowH3 = UI.CreateHorizontalLayoutGroup(vert) 
-	UI.CreateLabel(rowH3).SetText('Human AI can attack: ' .. tostring(M.Settings.H_city)).SetColor('#00B5FF')
+	UI.CreateLabel(rowH3).SetText('Human AI can attack: ' .. tostring(Mod.Settings.H_city)).SetColor('#00B5FF')
 
 	local rowH4 = UI.CreateHorizontalLayoutGroup(vert) 
-	UI.CreateLabel(rowH4).SetText('Human AI can attack: ' .. tostring(M.Settings.H_diplo)).SetColor('#00B5FF')
+	UI.CreateLabel(rowH4).SetText('Human AI can attack: ' .. tostring(Mod.Settings.H_diplo)).SetColor('#00B5FF')
 
 	local rowH5 = UI.CreateHorizontalLayoutGroup(vert) 
-	UI.CreateLabel(rowH5).SetText('Human AI can attack: ' .. tostring(M.Settings.H_block)).SetColor('#00B5FF')
+	UI.CreateLabel(rowH5).SetText('Human AI can attack: ' .. tostring(Mod.Settings.H_block)).SetColor('#00B5FF')
 
 	local rowH6 = UI.CreateHorizontalLayoutGroup(vert) 
-	UI.CreateLabel(rowH6).SetText('Human AI can attack: ' .. tostring(M.Settings.H_emergency)).SetColor('#00B5FF')
+	UI.CreateLabel(rowH6).SetText('Human AI can attack: ' .. tostring(Mod.Settings.H_emergency)).SetColor('#00B5FF')
 
 	local rowH7 = UI.CreateHorizontalLayoutGroup(vert) 
-	UI.CreateLabel(rowH7).SetText('Human AI can attack: ' .. tostring(M.Settings.H_rein)).SetColor('#00B5FF')
+	UI.CreateLabel(rowH7).SetText('Human AI can attack: ' .. tostring(Mod.Settings.H_rein)).SetColor('#00B5FF')
 
 	local rowH8 = UI.CreateHorizontalLayoutGroup(vert) 
-	UI.CreateLabel(rowH8).SetText('Human AI can attack: ' .. tostring(M.Settings.H_bomb .. '\n\n')).SetColor('#00B5FF')
+	UI.CreateLabel(rowH8).SetText('Human AI can attack: ' .. tostring(Mod.Settings.H_bomb .. '\n\n')).SetColor('#00B5FF')
 end
