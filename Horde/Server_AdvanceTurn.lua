ff72af541da29d79a9b1014f57aa8ce16e172796
larenz jackson <romanlegion15@gmail.com>
2024-08-14 10:08:46 -0500

    Update Server_AdvanceTurn.lua

--- Horde/Server_AdvanceTurn.lua
+++ Horde/Server_AdvanceTurn.lua
@@ -311,11 +311,12 @@ print(playerid,"test9090")
 print(playerid,"test9090")
 
 	for i = 1, #Mod.Settings.Slot do
-		if Mod.Settings.Slot[i] == Game2.Game.PlayingPlayers[playerid].Slot then
-			return true end
     if Mod.Settings.Slot[i] == -1 then 
       return true
-		end end
+		end
+    if Mod.Settings.Slot[i] == Game2.Game.PlayingPlayers[playerid].Slot then
+			return true end
+ end
 
       return issame
 end
